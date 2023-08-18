8dfe72e47189c1e41964117df9bf941c4afd0149
nitram509 <maki@bitkings.de>
2023-08-18 20:48:43 +0200

    flash helper

---
+++ martins-flasher.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+esptool.py --chip esp32s3 \
+    -p /dev/cu.usbmodem101 \
+    -b 460800 \
+    --before=default_reset \
+    --after=hard_reset \
+    write_flash 0x0 retro-go_1.39-pre-4-gb7cb0-dirty_flow3r.img
+
+
+# esptool.py --chip esp32s3 \
+#     -p /dev/cu.usbmodem101 \
+#     -b 460800 \
+#     --before=default_reset \
+#     write_flash 0x8000 ./launcher/build/partition_table/partition-table.bin
+
+
+
+
+#                  0x1000 bootloader/bootloader.bin \
+#                  0x10000 hello_world.bin
+
+# #   --flash_mode dio \
+# esptool.py -p /dev/cu.usbmodem1101 -b 460800 --before default_reset --after hard_reset --chip esp32  write_flash 0x90000 retro-go_1.39-pre-4-gb7cb0_flow3r.fw
